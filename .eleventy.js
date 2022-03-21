f527e52d169c3e18fab0cd38f3a6ec72df323fef
warugaki <warugaki.web.developer@gmail.com>
2022-03-21 14:56:53 +0900

    11tyに出力フォルダーにコピーするように

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,8 @@ module.exports = function(eleventyConfig) {
   
   // https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
   eleventyConfig.addWatchTarget("./src/scss/");
-  
+  // https://www.11ty.dev/docs/copy/#passthrough-file-copy
+  eleventyConfig.addPassthroughCopy("./src/css");
   
   // https://www.11ty.dev/docs/config/
   return {
